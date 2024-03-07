090ee08c41176985ccb4808d04ad4d69cbc74627
Akshay Rathore <akshayrathore1197@gmail.com>
2024-03-07 21:36:06 +0530

    march7

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"  //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+
+    
+    3); //code readability should be high
+
+
+console.log("Akshay");
+
+let name = "Akshay"
+let age = 19
+let isLoggedin = true
+
+// numer => 2 to power 53
+// bigint
+// string => " "
+// Boolean =>true/false
+//null => standalone value
+// undefined => value not assigned
+// null => value not assigned
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
