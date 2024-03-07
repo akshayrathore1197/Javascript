9d3169f1f404775fbd7dfc2575a3c88b3a16e64c
Akshay Rathore <akshayrathore1197@gmail.com>
2024-03-07 21:51:26 +0530

    march7

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "33abc";
+// console.log(score);
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//! "33" => 33
+// !"33abc" => NaN
+
+// let isLoggedIn = "akshay"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//! 1 => true; 0 => false
+//! "" => false
+//! "akshay" => true
+
+
+let someNum = 0
+let stringNum = String(someNum)
+console.log(stringNum);
