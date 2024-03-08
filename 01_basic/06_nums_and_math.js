2575ab2c8538d98b0e314748a5dd18883426488e
Akshay Rathore <akshayrathore1197@gmail.com>
2024-03-08 14:42:30 +0530

    march8

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400;
+const bal = new Number(100.8);
+const hundreds = 1000000;
+const otherNum = 23.9456;
+
+// console.log(score);
+
+// console.log(bal);
+
+// console.log(bal.toString().length);
+// console.log(bal.toFixed(2));
+
+// console.log(otherNum.toPrecision(3));
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+
+
+// ! ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+console.log(Math);
+
+// console.log(Math.abs(-9545));
+// console.log(Math.round(45.655));
+// console.log(Math.ceil(45.655));
+// console.log(Math.floor(45.655));
+// console.log(Math.min(4,8,5,4,5));
+// console.log(Math.max(4,8,5,4,5));
+
+// console.log(Math.random());
+// console.log((Math.random()*10 )+ 1);
+// console.log(Math.floor((Math.random()*10 )+ 1));
+
+const min =10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min);
